0fad13db09c2a6365927066952fc27a1440be40e
Stamatis Tzanos <stzanos@inaccess.com>
2022-12-20 21:18:06 +0200

    Merge OneDict into master

--- Day2/rock_paper_scissors_strategy.py
+++ Day2/rock_paper_scissors_strategy.py
@@ -1,24 +1,23 @@
-selection_scores = {'X': 1, 'Y': 2, 'Z': 3}
-draw_games = [('A', 'X'), ('B', 'Y'), ('C', 'Z')]
-win_games = [('A', 'Z'), ('B', 'X'), ('C', 'Y')]
+outcomes = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6
+}
 
 class RockPaperScissorsTournament:
 
     def __init__(self, games_filename: str):
         with open(games_filename, 'r') as f:
-            self._games = [game_line.split(' ') for game_line in f.read().splitlines()]
+            self._games = [game_line for game_line in f.read().splitlines()]
 
     def get_total_score(self):
-        return sum([self._get_game_score(*game) for game in self._games])
-
-    @staticmethod
-    def _get_game_score(opponent_selection: str, player_selection: str) -> float:
-        score = selection_scores[player_selection]
-        if (opponent_selection, player_selection) in draw_games:
-            score += 3
-        elif (opponent_selection, player_selection) in win_games:
-            score += 6
-        return score
+        return sum([outcomes[game] for game in self._games])
 
 
 if __name__ == '__main__':
