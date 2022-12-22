b29657683413c855b9b26c0713ee2eb4a400181c
Stamatis Tzanos <stzanos@inaccess.com>
2022-12-22 10:10:29 +0200

    Put all crates in a list of lists

--- Day5/supply_stacks.py
+++ Day5/supply_stacks.py
@@ -1,3 +1,6 @@
+from more_itertools import chunked
+
+
 class CargoCrane:
 
     def __init__(self, filename):
@@ -5,13 +8,33 @@ class CargoCrane:
             self._data = f.read().splitlines()
         self._stacks = self._get_stacks()
         self._directions = self._get_directions()
+        self._move_crates()
 
-    def _get_stacks(self) -> {list}:
-        return {'': self._data}
+    def _move_crates(self):
+        pass
+
+    def _get_stacks(self) -> list:
+        crate_rows = self._data[:self._get_stack_endpoint_idx()]
+        crate_stacks = []
+        for crate_row in crate_rows[::-1]:
+            for i, crate in enumerate(chunked(crate_row, 4)):
+                crate_str = ''.join(crate).strip()
+                if crate_str != '':
+                    try:
+                        crate_stacks[i].append(crate_str)
+                    except IndexError:
+                        crate_stacks.append([crate_str])
+        return crate_stacks
 
     def _get_directions(self) -> {list}:
         return {'': self._data}
 
+    def _get_stack_endpoint_idx(self) -> int:
+        for i, line in enumerate(self._data):
+            if '1' in line:
+                return i
+        return 0
+
 
 if __name__ == '__main__':
-    pass
+    crane = CargoCrane('stacks_of_crates.txt')
