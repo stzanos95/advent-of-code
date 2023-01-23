61dbfff66aa32a96bf19cb4a6672707aef9ce4ed
stzanos95 <stamatios.tzanos@gmail.com>
2023-01-23 10:10:32 +0200

    Optimized in one function.

--- Day10/cathoderay_tube.py
+++ Day10/cathoderay_tube.py
@@ -4,6 +4,7 @@ class CathodeRayTube:
 
     def __init__(self, program_file: str):
         self._cycles = 1
+        self._graphic_cycles = 1
         self._x = 1
         self._strength_sum = 0
         self._screen = [[], [], [], [], [], [], []]
@@ -11,54 +12,44 @@ class CathodeRayTube:
         with open(program_file, 'r') as f:
             self._program = f.read().splitlines()
 
-    def compile_program_strength(self) -> int:
+    def compile_program(self) -> int:
+        """
+
+        Compiles the program and prints the rendered image.
+        :return: The sum of the signal strengths.
+
+        """
         for command in self._program:
             self._process_command(command)
-        return self._strength_sum
-
-    def render_image(self):
-        for command in self._program:
-            self._render_pixels(command)
         for line in self._screen:
             print(''.join(line))
+        return self._strength_sum
 
     def _process_command(self, command):
         if command == 'noop':
             self._check_interesting()
+            self._update_screen()
             self._next_cycle()
         else:
             v = int(command.split()[1])
             for _ in range(2):
                 self._check_interesting()
+                self._update_screen()
                 self._next_cycle()
             self._x += v
 
-    def _render_pixels(self, command: str):
-        if command == 'noop':
-            self._update_screen()
-            self._next_cycle(render=True)
-        else:
-            v = int(command.split()[1])
-            for _ in range(2):
-                self._update_screen()
-                self._next_cycle(render=True)
-            self._x += v
-
     def _update_screen(self):
-        if self._cycles in range(self._x, self._x + 3):
+        if self._graphic_cycles in range(self._x, self._x + 3):
             self._screen[self._row].append('#')
         else:
             self._screen[self._row].append('.')
 
-    def _next_cycle(self, render=False):
-        if not render:
-            self._cycles += 1
-        else:
-            print(self._cycles)
-            self._cycles += 1
-            if self._cycles > 40:
-                self._cycles = self._cycles % 40
-                self._row += 1
+    def _next_cycle(self):
+        self._cycles += 1
+        self._graphic_cycles += 1
+        if self._graphic_cycles > 40:
+            self._graphic_cycles = self._graphic_cycles % 40
+            self._row += 1
 
     def _check_interesting(self):
         if self._cycles in self._interesting_cycles:
@@ -66,7 +57,5 @@ class CathodeRayTube:
 
 
 if __name__ == '__main__':
-    tube1 = CathodeRayTube('program.txt')
-    print(tube1.compile_program_strength())
-    tube2 = CathodeRayTube('program.txt')
-    tube2.render_image()
+    tube = CathodeRayTube('program.txt')
+    print(tube.compile_program())
